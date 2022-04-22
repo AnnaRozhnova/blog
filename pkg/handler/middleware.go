b6089af60c64b90f8e4d04eef4ccf2c442fb7097
AnnaRozhnova <rozhnova.av.19@gmail.com>
2022-04-22 21:31:13 +0300

    *

--- pkg/handler/middleware.go
+++ pkg/handler/middleware.go
@@ -5,6 +5,6 @@ import "github.com/gin-gonic/gin"
 
 func (h *Handler) addHeader(c *gin.Context) {
 	c.Header("Access-Control-Allow-Origin", "*")
-	c.Header("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	//c.Header("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 }
