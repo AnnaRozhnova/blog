5f89fbbebbb2579e5feddd927e0c19b2b1b150b8
AnnaRozhnova <rozhnova.av.19@gmail.com>
2022-05-28 13:19:07 +0300

    Post struct edited

--- blog.go
+++ blog.go
@@ -1,16 +1,15 @@
 package blog
 
 type Post struct {
-	Id     int    `json:"-" db:"id"`
-	Title  string `json:"title" db:"title" binding:"required"`
-	Body   string `json:"body" db:"body" binding:"required"`
-	Username string    `json:"username" db:"username"`
+	Id       int    `json:"id" db:"id"`
+	Title    string `json:"title" db:"title" binding:"required"`
+	Body     string `json:"body" db:"body" binding:"required"`
+	Username string `json:"username" db:"username"`
 }
 
-
 type Comment struct {
-	Id int `json:"-" db:"id"`
-	Body string `json:"body" db:"body" binding:"required"`
+	Id       int    `json:"-" db:"id"`
+	Body     string `json:"body" db:"body" binding:"required"`
 	Username string `json:"username" db:"username"`
-	PostId int `json:"post_id" db:"post_id" binding:"required"`
+	PostId   int    `json:"post_id" db:"post_id" binding:"required"`
 }
